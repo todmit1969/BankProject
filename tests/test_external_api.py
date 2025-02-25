46b0ece0387b4c75e93ea3e256ab5c66475a3adc
tmitev <todor.mitev@sofairo.com>
2025-02-25 17:25:16 +0300

    Test_external_api works

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -2,15 +2,21 @@ from unittest.mock import patch, Mock
 from src.external_api import convert_rub
 
 @patch("requests.get")
-def test_convert_rub(mock_get, convert_rub=7000):
+def test_convert_rub(mock_get, convert_to_rub=7000):
     mock_response = Mock()
     mock_response.status_code = 200
     mock_response.json.return_value = {"rates":{"RUB":70}}
     mock_get.return_value = mock_response
 
-    transaction = [{"amount":100, "currency":"USD"}]
+    transaction = {"operationAmount": {
+                      "amount": "100",
+                      "currency": {
+                        "name": "USD",
+                        "code": "USD"
+                      }
+                    }}
 
     result = convert_rub(transaction)
 
-    assert convert_rub == result
+    assert convert_to_rub == result
     mock_get.assert_called_once()
